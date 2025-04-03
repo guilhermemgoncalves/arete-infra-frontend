01c994f9047248e977e595c0f7a3142e485200f3
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-03 08:35:56 -0300

    feat: initialize comming soon page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {SplashScreenComponent} from './core/components/splash-screen/splash-screen.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SplashScreenComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
