e5cb6dbb1bafbbb79bc6b6beb5bf859265cbc4fc
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-07 07:20:55 -0300

    feat: initialize section tags and put id tags foreach landing-page section

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {VideoPresentationComponent} from './sections/video-presentation/video-presentation.component';
-import {CustomersComponent} from './sections/customers/customers.component';
-import {UtilityCompaniesComponent} from './sections/utility-companies/utility-companies.component';
-import {ServicesComponent} from './sections/services/services.component';
-import {InstaGalleryComponent} from './sections/insta-gallery/insta-gallery.component';
-import {ContactsComponent} from './sections/contacts/contacts.component';
+import { VideoPresentationComponent } from './sections/video-presentation/video-presentation.component';
+import { CustomersComponent } from './sections/customers/customers.component';
+import { UtilityCompaniesComponent } from './sections/utility-companies/utility-companies.component';
+import { ServicesComponent } from './sections/services/services.component';
+import { InstaGalleryComponent } from './sections/insta-gallery/insta-gallery.component';
+import { ContactsComponent } from './sections/contacts/contacts.component';
+import { Meta, Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'landing-page',
@@ -17,8 +18,16 @@ import {ContactsComponent} from './sections/contacts/contacts.component';
     ContactsComponent
   ],
   templateUrl: './landing-page.component.html',
-  styleUrl: './landing-page.component.scss'
+  styleUrls: ['./landing-page.component.scss']
 })
 export class LandingPageComponent {
 
+  constructor(
+    private metaService: Meta,
+    private titleService: Title
+  ) {
+    this.titleService.setTitle('Areté - Landing Page');
+    this.metaService.updateTag({ name: 'description', content: 'Areté infraestrutura landing page' });
+    this.metaService.updateTag({ name: 'keywords', content: 'areté, infraestrutura, areté-infra, engenharia, saneamento, esgoto, pluviais, portifólio, estação elevatória, agua' });
+  }
 }
