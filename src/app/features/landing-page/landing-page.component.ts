2b4a3804c7ee9a4c37fc670bf0c83e8425224ec3
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-07 06:53:30 -0300

    feat: initialize landing-page.component.ts sections

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,8 +1,21 @@
 import { Component } from '@angular/core';
+import {VideoPresentationComponent} from './sections/video-presentation/video-presentation.component';
+import {CustomersComponent} from './sections/customers/customers.component';
+import {UtilityCompaniesComponent} from './sections/utility-companies/utility-companies.component';
+import {ServicesComponent} from './sections/services/services.component';
+import {InstaGalleryComponent} from './sections/insta-gallery/insta-gallery.component';
+import {ContactsComponent} from './sections/contacts/contacts.component';
 
 @Component({
   selector: 'landing-page',
-  imports: [],
+  imports: [
+    VideoPresentationComponent,
+    CustomersComponent,
+    UtilityCompaniesComponent,
+    ServicesComponent,
+    InstaGalleryComponent,
+    ContactsComponent
+  ],
   templateUrl: './landing-page.component.html',
   styleUrl: './landing-page.component.scss'
 })
