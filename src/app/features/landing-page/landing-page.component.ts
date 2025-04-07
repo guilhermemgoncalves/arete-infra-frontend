7f8e667eadf19aea6b9d4169b3eab388c12ae73c
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-07 07:34:21 -0300

    SEO: improve landing page keywords

--- src/app/features/landing-page/landing-page.component.ts
+++ src/app/features/landing-page/landing-page.component.ts
@@ -1,11 +1,11 @@
-import { Component } from '@angular/core';
-import { VideoPresentationComponent } from './sections/video-presentation/video-presentation.component';
-import { CustomersComponent } from './sections/customers/customers.component';
-import { UtilityCompaniesComponent } from './sections/utility-companies/utility-companies.component';
-import { ServicesComponent } from './sections/services/services.component';
-import { InstaGalleryComponent } from './sections/insta-gallery/insta-gallery.component';
-import { ContactsComponent } from './sections/contacts/contacts.component';
-import { Meta, Title } from '@angular/platform-browser';
+import {Component} from '@angular/core';
+import {VideoPresentationComponent} from './sections/video-presentation/video-presentation.component';
+import {CustomersComponent} from './sections/customers/customers.component';
+import {UtilityCompaniesComponent} from './sections/utility-companies/utility-companies.component';
+import {ServicesComponent} from './sections/services/services.component';
+import {InstaGalleryComponent} from './sections/insta-gallery/insta-gallery.component';
+import {ContactsComponent} from './sections/contacts/contacts.component';
+import {Meta, Title} from '@angular/platform-browser';
 
 @Component({
   selector: 'landing-page',
@@ -27,7 +27,14 @@ export class LandingPageComponent {
     private titleService: Title
   ) {
     this.titleService.setTitle('Areté - Landing Page');
-    this.metaService.updateTag({ name: 'description', content: 'Areté infraestrutura landing page' });
-    this.metaService.updateTag({ name: 'keywords', content: 'areté, infraestrutura, areté-infra, engenharia, saneamento, esgoto, pluviais, portifólio, estação elevatória, agua' });
+    this.metaService.updateTag({name: 'description', content: 'Areté infraestrutura landing page'});
+    this.metaService.updateTag({
+      name: 'keywords',
+      content: 'areté, infraestrutura, areté-infra, engenharia, saneamento, esgoto, pluviais, portifólio, ' +
+        'estação elevatória, agua, Construção civil, obras públicas, urbanismo, hidráulica, tratamento de água, ' +
+        'tratamento de esgoto, drenagem urbana, recursos hídricos, rede de abastecimento, eficiência hídrica, ' +
+        'meio ambiente, sustentabilidade, projetos estruturais, infraestrutura urbana, engenharia ambiental, ' +
+        'sistemas de bombeamento, reservatórios, tubulações, gestão de resíduos, qualidade da água, modernização urbana'
+    });
   }
 }
