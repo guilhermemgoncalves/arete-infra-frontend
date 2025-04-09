69a584f50497dd6bdc3c20383fc2787b16b84120
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-08 21:21:50 -0300

    create video presentation video rendering

--- src/app/features/landing-page/sections/video-presentation/video-presentation.component.ts
+++ src/app/features/landing-page/sections/video-presentation/video-presentation.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {DomSanitizer} from '@angular/platform-browser';
 
 @Component({
   selector: 'video-presentation',
@@ -7,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './video-presentation.component.scss'
 })
 export class VideoPresentationComponent {
+
+  url = 'https://www.youtube.com/embed/Dh7HJdPKVaY?si=0jBvFFZRrhND01Ae' +
+    '?modesbranding=1&rel=0&showinfo=0&autoplay=1&mute=1&cc_load_policy=0&loop=1';
+  safeUrl
+
+  constructor(private sanitizer: DomSanitizer) {
+    this.safeUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.url);
+  }
 
 }
