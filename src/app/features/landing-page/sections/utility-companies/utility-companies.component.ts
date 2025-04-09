c874ae35ba305087d5bbf643671cc171b74e304d
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-08 23:31:23 -0300

    feat: creeate fade in on scroll directive

--- src/app/features/landing-page/sections/utility-companies/utility-companies.component.ts
+++ src/app/features/landing-page/sections/utility-companies/utility-companies.component.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import {FadeInOnScrollDirective} from '../../../../core/directives/fade-in-on-scroll.directive';
 
 @Component({
   selector: 'utility-companies',
-  imports: [],
+  imports: [
+    FadeInOnScrollDirective
+  ],
   templateUrl: './utility-companies.component.html',
   styleUrl: './utility-companies.component.scss'
 })
