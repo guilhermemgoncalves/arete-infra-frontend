1eff899ff27d609cbb5ca94689ecd8fb73d77af7
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-11 23:59:16 -0300

    feat: section style first layout implementation

--- src/app/features/landing-page/sections/services/services.component.ts
+++ src/app/features/landing-page/sections/services/services.component.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import {FadeInOnScrollDirective} from '../../../../core/directives/fade-in-on-scroll.directive';
 
 @Component({
   selector: 'services',
-  imports: [],
+  imports: [
+    FadeInOnScrollDirective
+  ],
   templateUrl: './services.component.html',
   styleUrl: './services.component.scss'
 })
 export class ServicesComponent {
+  servicesText!: string;
 
+  constructor() {
+    this.servicesText = `
+      <p>Somos uma empresa totalmente focada na satifsa&ccedil;&atilde;o do cliente e contamos com os seguintes servi&ccedil;os:</p>
+      <p>Somos uma empresa totalmente focada na satifsa&ccedil;&atilde;o do cliente e contamos com os seguintes servi&ccedil;os:</p>
+      <ul>
+      <li>Dimensionamento de redes de esgoto</li>
+      <li>Projetos conceituais e executivos</li>
+      <li>Valida&ccedil;&atilde;o nas principais concessionarias do Brasil</li>
+      <li>Consultoria em servi&ccedil;os de infraestrutura</li>
+      <li>Projetos de esta&ccedil;&atilde;o elevat&oacute;ria</li>
+      </ul>
+    `;
+  }
 }
