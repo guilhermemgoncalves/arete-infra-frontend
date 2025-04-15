ee5bc1957cd1836af7b4f10e96ec7095034ef062
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-15 10:17:09 -0300

    feat: environments

--- src/app/features/landing-page/sections/insta-gallery/insta-gallery.component.ts
+++ src/app/features/landing-page/sections/insta-gallery/insta-gallery.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import {ImageService} from '../../../../core/services/image/image.service';
 import {ImageUrlDownloadPipe} from '../../../../core/pipes/image-url-download.pipe';
 import {InstagramImageDto} from './instagram-image.dto';
 
@@ -17,6 +16,11 @@ export class InstaGalleryComponent {
 
   images: Array<InstagramImageDto> =  [
     {
+      key: '62a753a8-d733-4a50-8f98-943889c8ff94',
+      link: 'https://www.instagram.com/p/DBcmW7psNc_/?img_index=1',
+      likes: 10,
+      comments: 12
+    }, {
       key: '62a753a8-d733-4a50-8f98-943889c8ff94',
       link: 'https://www.instagram.com/p/DBcmW7psNc_/?img_index=1',
       likes: 10,
