0ac53c8fe212a428138607032f9213a8c1b03df6
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-16 11:35:54 -0300

    feat: include title and global section divider

--- src/app/features/landing-page/sections/insta-gallery/insta-gallery.component.ts
+++ src/app/features/landing-page/sections/insta-gallery/insta-gallery.component.ts
@@ -19,22 +19,22 @@ export class InstaGalleryComponent {
       key: '62a753a8-d733-4a50-8f98-943889c8ff94',
       link: 'https://www.instagram.com/p/DBcmW7psNc_/?img_index=1',
       likes: 10,
-      comments: 12
+      comments: 12,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
     }, {
       key: '62a753a8-d733-4a50-8f98-943889c8ff94',
       link: 'https://www.instagram.com/p/DBcmW7psNc_/?img_index=1',
       likes: 10,
-      comments: 12
+      comments: 1,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+
     }, {
       key: '62a753a8-d733-4a50-8f98-943889c8ff94',
       link: 'https://www.instagram.com/p/DBcmW7psNc_/?img_index=1',
       likes: 10,
-      comments: 12
-    }, {
-      key: '62a753a8-d733-4a50-8f98-943889c8ff94',
-      link: 'https://www.instagram.com/p/DBcmW7psNc_/?img_index=1',
-      likes: 10,
-      comments: 12
+      comments: 1,
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
     }
   ]
 }
