b45896c47974e256e5aa99aa0129fe3985b6dae2
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-19 17:29:18 -0300

    feat: initialize image galery component

---
+++ src/app/core/components/image-selector/image-dto.ts
@@ -0,0 +1,7 @@
+
+export interface ImageDto {
+  key: string;
+  extension: string;
+  name: string;
+  selected?: boolean;
+}
---
+++ src/app/core/components/image-selector/image-selector.component.ts
@@ -0,0 +1,42 @@
+import {Component} from '@angular/core';
+import {ImageUrlDownloadPipe} from "../../pipes/image-url-download.pipe";
+import {FormsModule} from '@angular/forms';
+import {ImageDto} from './image-dto';
+import {ImageService} from '../../services/image/image.service';
+import {Endpoints} from '../../enums/enpoint-enums';
+
+@Component({
+  selector: 'app-image-selector',
+  imports: [
+    ImageUrlDownloadPipe,
+    FormsModule
+  ],
+  templateUrl: './image-selector.component.html',
+  styleUrl: './image-selector.component.scss'
+})
+export class ImageSelectorComponent {
+  images : Array<ImageDto> = [];
+  endpoints: Endpoints[] = Object.values(Endpoints);
+  selectedEndpoint: Endpoints = Endpoints.INSTAGRAM;
+
+  constructor(private imageService: ImageService) {
+    this.getImages().then(console.log);
+  }
+
+  async getImages() {
+    this.images = await this.imageService.listImages();
+  }
+
+
+  saveImages(){
+    const imagesToSave = this.images.filter(image => image.selected);
+    this.imageService.selectImages(imagesToSave, this.selectedEndpoint);
+    console.log(imagesToSave);
+  }
+
+  getEndpointName(endpoint: string): string {
+    return endpoint.replace('/', '').split('-')
+      .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+      .join(' ');
+  }
+}
