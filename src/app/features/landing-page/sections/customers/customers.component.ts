cd19bc95d64fbc311117bb53372f45a4bfa1aa35
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-04-27 10:16:58 -0300

    feat: implements customers and companies layout

--- src/app/features/landing-page/sections/customers/customers.component.ts
+++ src/app/features/landing-page/sections/customers/customers.component.ts
@@ -11,4 +11,15 @@ import {GroupingLogoComponent} from '../../../../core/components/grouping-logo/g
 })
 export class CustomersComponent {
 
+  customersText: string = `
+      <p>Somos uma empresa totalmente focada na satisfa&ccedil;&atilde;o do cliente e contamos com os seguintes servi&ccedil;os:</p>
+      <ul>
+        <li>Dimensionamento de redes de esgoto</li>
+        <li>Projetos conceituais e executivos</li>
+        <li>Valida&ccedil;&atilde;o nas principais concessionarias do Brasil</li>
+        <li>Consultoria em servi&ccedil;os de infraestrutura</li>
+        <li>Projetos de esta&ccedil;&atilde;o elevat&oacute;ria</li>
+      </ul>
+    `;
+
 }
--- src/app/features/landing-page/sections/utility-companies/utility-companies.component.ts
+++ src/app/features/landing-page/sections/utility-companies/utility-companies.component.ts
@@ -1,14 +1,26 @@
 import { Component } from '@angular/core';
 import {FadeInOnScrollDirective} from '../../../../core/directives/fade-in-on-scroll.directive';
+import {GroupingLogoComponent} from "../../../../core/components/grouping-logo/grouping-logo.component";
 
 @Component({
   selector: 'utility-companies',
-  imports: [
-    FadeInOnScrollDirective
-  ],
+    imports: [
+        FadeInOnScrollDirective,
+        GroupingLogoComponent
+    ],
   templateUrl: './utility-companies.component.html',
   styleUrl: './utility-companies.component.scss'
 })
 export class UtilityCompaniesComponent {
 
+  companiesText: string = `
+      <p>Somos uma empresa totalmente focada na satisfa&ccedil;&atilde;o do cliente e contamos com os seguintes servi&ccedil;os:</p>
+      <ul>
+        <li>Dimensionamento de redes de esgoto</li>
+        <li>Projetos conceituais e executivos</li>
+        <li>Valida&ccedil;&atilde;o nas principais concessionarias do Brasil</li>
+        <li>Consultoria em servi&ccedil;os de infraestrutura</li>
+        <li>Projetos de esta&ccedil;&atilde;o elevat&oacute;ria</li>
+      </ul>
+    `;
 }
