5d126e765104aa41aab1b55301bcfb6b36f2e713
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-05-08 06:58:22 -0300

    feat: layout adjusments

--- src/app/features/landing-page/sections/insta-gallery/insta-gallery.component.ts
+++ src/app/features/landing-page/sections/insta-gallery/insta-gallery.component.ts
@@ -1,11 +1,15 @@
 import {Component, input} from '@angular/core';
 import {ImageUrlDownloadPipe} from '../../../../core/pipes/image-url-download.pipe';
 import {InstagramImageDto} from '../../../../core/dtos/instagram-image.dto';
+import {FadeInOnScrollDirective} from '../../../../core/directives/fade-in-on-scroll.directive';
+import {ContactsComponent} from '../contacts/contacts.component';
 
 @Component({
   selector: 'insta-gallery',
   imports: [
-    ImageUrlDownloadPipe
+    ImageUrlDownloadPipe,
+    FadeInOnScrollDirective,
+    ContactsComponent
   ],
   templateUrl: './insta-gallery.component.html',
   styleUrl: './insta-gallery.component.scss'
