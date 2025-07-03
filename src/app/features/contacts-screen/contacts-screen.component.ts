0db5e1bcf6db2b38744512cbe6191edf33a2cbd2
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-07-03 19:50:34 -0300

    feat: add Azure identity and Microsoft Graph client dependencies

---
+++ src/app/features/contacts-screen/contacts-screen.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import {ComingSoonComponent} from '../../core/components/coming-soon/coming-soon.component';
+
+@Component({
+  selector: 'app-contacts-screen',
+  imports: [
+    ComingSoonComponent
+  ],
+  templateUrl: './contacts-screen.component.html',
+  styleUrl: './contacts-screen.component.scss'
+})
+export class ContactsScreenComponent {
+
+}
