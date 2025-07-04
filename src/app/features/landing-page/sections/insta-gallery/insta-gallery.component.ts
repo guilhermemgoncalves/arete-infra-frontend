6c64e55324c7795647d7ef48b207f40ed7fb1ecd
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-07-04 08:30:54 -0300

    feat: enhance styling and layout of header and gallery components

--- src/app/features/landing-page/sections/insta-gallery/insta-gallery.component.ts
+++ src/app/features/landing-page/sections/insta-gallery/insta-gallery.component.ts
@@ -2,14 +2,12 @@ import {Component, input} from '@angular/core';
 import {ImageUrlDownloadPipe} from '../../../../core/pipes/image-url-download.pipe';
 import {InstagramImageDto} from '../../../../core/dtos/instagram-image.dto';
 import {FadeInOnScrollDirective} from '../../../../core/directives/fade-in-on-scroll.directive';
-import {ContactsComponent} from '../contacts/contacts.component';
 
 @Component({
   selector: 'insta-gallery',
   imports: [
     ImageUrlDownloadPipe,
-    FadeInOnScrollDirective,
-    ContactsComponent
+    FadeInOnScrollDirective
   ],
   templateUrl: './insta-gallery.component.html',
   styleUrl: './insta-gallery.component.scss'
