995acead96bbe5da0a8261ee5971be68c2820984
guilherme.goncalves <guilherme_marciano@outlook.com.br>
2025-09-18 14:29:39 -0300

    feat: refactor customers and utility companies sections with improved layout and styling

--- src/app/features/landing-page/sections/customers/customers.component.ts
+++ src/app/features/landing-page/sections/customers/customers.component.ts
@@ -1,11 +1,13 @@
 import {Component, input} from '@angular/core';
 import {GroupingLogoComponent} from '../../../../core/components/grouping-logo/grouping-logo.component';
 import {LogoImageDto} from '../../../../core/dtos/log-image.dto';
+import {FadeInOnScrollDirective} from '../../../../core/directives/fade-in-on-scroll.directive';
 
 @Component({
   selector: 'customers',
   imports: [
-    GroupingLogoComponent
+    GroupingLogoComponent,
+    FadeInOnScrollDirective
   ],
   templateUrl: './customers.component.html',
   styleUrl: './customers.component.scss'
